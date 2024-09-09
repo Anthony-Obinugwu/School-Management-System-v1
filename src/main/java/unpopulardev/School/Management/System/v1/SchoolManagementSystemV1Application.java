c50576c19cc902f4f67606d7000891ecb6378c1d
htmlkill <110816510+htmlkill@users.noreply.github.com>
2024-09-09 12:13:26 +0100

    Resolved Merge Conflicts

---
+++ src/main/java/unpopulardev/School/Management/System/v1/SchoolManagementSystemV1Application.java
@@ -0,0 +1,13 @@
+package unpopulardev.School.Management.System.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemV1Application.class, args);
+	}
+
+}
